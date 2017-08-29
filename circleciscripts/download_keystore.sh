edf7d1ad0324f3d73197af17df1c3926ef3c5214
asksven <sven.knispel@gmail.com>
2017-08-29 10:26:52 +0200

    Added CircleCI scripts

---
+++ circleciscripts/download_keystore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+curl -O ${KEYSTORE_URI}/${KEYSTORE_RELEASE}-cipher
+curl -O ${KEYSTORE_URI}/${KEYSTORE_DEBUG}-cipher
+
+if [ -e ${KEYSTORE_RELEASE}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $HOME/unclutter/app/$KEYSTORE_RELEASE; fi
+if [ -e ${KEYSTORE_DEBUG}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $HOME/unclutter/app/$KEYSTORE_DEBUG; fi
+
---
+++ circleciscripts/renameapks.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+for filename in $(find $HOME/unclutter/app/build/outputs/apk -name "*.apk"); do rename -v 's/(.*)\.apk/$1-$ENV{"CIRCLE_BUILD_NUM"}.apk/' $filename; done;
