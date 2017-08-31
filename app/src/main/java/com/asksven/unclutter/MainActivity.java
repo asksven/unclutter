1d52ef0df8e4766e19c50d90fd17b6fa368f51aa
asksven <sven.knispel@gmail.com>
2017-08-31 20:05:05 +0200

    First experimental code for testing purposes

--- app/src/main/java/com/asksven/unclutter/MainActivity.java
+++ app/src/main/java/com/asksven/unclutter/MainActivity.java
@@ -1,16 +1,42 @@
 package com.asksven.unclutter;
 
+import android.annotation.TargetApi;
+import android.app.usage.UsageStats;
+import android.app.usage.UsageStatsManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import net.hockeyapp.android.CrashManager;
+import net.hockeyapp.android.Tracking;
+import net.hockeyapp.android.UpdateManager;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity
 {
+    static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -32,6 +58,77 @@ public class MainActivity extends AppCompatActivity
         });
     }
 
+    /* Request updates at startup */
+    @Override
+    protected void onResume()
+    {
+        super.onResume();
+        Log.i(TAG, "OnResume called");
+
+        // we need to make sure that hockeyapp actions never crash the app
+        try
+        {
+            CrashManager.register(this);
+            Tracking.startUsage(this);
+
+            PackageInfo pinfo = getPackageManager().getPackageInfo(getPackageName(), 0);
+            if (pinfo.packageName.endsWith("_hockeyappedition"))
+            {
+                UpdateManager.register(this);
+            }
+        }
+        catch (Exception e)
+        {
+        }
+
+        // Analytics opt-in
+        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+
+        boolean wasPresentedOptOutAnalytics = prefs.getBoolean("analytics_opt_out_offered", false);
+
+        if (!wasPresentedOptOutAnalytics)
+        {
+            Log.i(TAG, "Application was launched for the first time with analytics");
+
+            Snackbar bar = Snackbar.make(findViewById(android.R.id.content), R.string.pref_app_analytics_summary, Snackbar.LENGTH_LONG)
+                    .setAction(R.string.label_button_no, new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            SharedPreferences.Editor editor = prefs.edit();
+                            editor.putBoolean("analytics", false);
+                            editor.commit();
+                        }
+                    });
+
+            bar.show();
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putBoolean("analytics_opt_out_offered", true);
+            editor.commit();
+        }
+
+
+    }
+
+    /* Remove the locationlistener updates when Activity is paused */
+    @Override
+    protected void onPause()
+    {
+        super.onPause();
+        // Hockeyapp
+        UpdateManager.unregister();
+        Tracking.stopUsage(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        // Hockeyapp
+        UpdateManager.unregister();
+    }
+
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu)
     {
@@ -40,7 +137,9 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
+    // For former Androif versions see: https://developer.android.com/reference/android/app/ActivityManager.html#getRecentTasks(int, int)
     @Override
+    @TargetApi(21)
     public boolean onOptionsItemSelected(MenuItem item)
     {
         // Handle action bar item clicks here. The action bar will
@@ -53,6 +152,43 @@ public class MainActivity extends AppCompatActivity
         {
             return true;
         }
+
+        if (id == R.id.action_test)
+        {
+            final PackageManager pm = getPackageManager();
+            final UsageStatsManager um=(UsageStatsManager)this.getSystemService("usagestats");// Context.USAGE_STATS_SERVICE);
+
+            //get a list of installed apps.
+            List<ApplicationInfo> applications = pm.getInstalledApplications(PackageManager.GET_META_DATA);
+            List<PackageInfo> packages = pm.getInstalledPackages(PackageManager.GET_META_DATA);
+            long now = System.currentTimeMillis();
+
+            Map<String, UsageStats> usageStats = um.queryAndAggregateUsageStats(now - (365*24*60*60*1000), now);
+
+            for (PackageInfo packageInfo : packages)
+            {
+                // Filter out system apps
+                if ((packageInfo.applicationInfo.flags & ApplicationInfo.FLAG_SYSTEM) == 0)
+                {
+                    Log.d(TAG, "Installed package :" + packageInfo.packageName);
+//                    Log.d(TAG, "Installed first :" + DateUtils.formatDurationLong(now - packageInfo.firstInstallTime));
+                    Log.d(TAG, "Last updated :" + DateUtils.formatDurationLong(now - packageInfo.lastUpdateTime));
+                    UsageStats us = (UsageStats) usageStats.get(packageInfo.packageName);
+                    if (us != null)
+                    {
+                        Log.d(TAG, "Last used :" + DateUtils.formatDurationLong(now - us.getLastTimeUsed()));
+                        Log.d(TAG, "Time spend in foregroud :" + DateUtils.formatDurationLong(us.getTotalTimeInForeground()));
+                    }
+                    else
+                    {
+                        Log.d(TAG, "No usage stats found for " + packageInfo.packageName);
+                    }
+//                    Log.d(TAG, "Source dir : " + packageInfo.sourceDir);
+//                    Log.d(TAG, "Launch Activity :" + pm.getLaunchIntentForPackage(packageInfo.packageName));
+                }
+            }
+        }
         return super.onOptionsItemSelected(item);
     }
+
 }
