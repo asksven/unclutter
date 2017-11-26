c9e2bb293c71484decb2550b273f93215892bc63
asksven <sven.knispel@gmail.com>
2017-11-26 18:02:17 +0100

    An attempt to fix env-vars to be persistent to all commands

--- circleciscripts/decrypt_env_vars.sh
+++ circleciscripts/decrypt_env_vars.sh
@@ -4,10 +4,5 @@
 # openssl aes-256-cbc -e -in secret-env-plain -out secret-env-cipher -k KEY
 
 echo "Decrypting"
-openssl aes-256-cbc -d -md md5 -in secret-env-cipher -k $KEY >> ~/.circlerc
+openssl aes-256-cbc -d -md md5 -in secret-env-cipher -out secret-env-plain -k $KEY
 ls -l secret-*
-cat ~/.circlerc
-source ~/.circlerc
-
-echo "Setting vars"
-echo $KEY_ALIAS
