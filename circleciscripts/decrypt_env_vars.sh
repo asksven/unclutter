83c91c0400d47b78cd0e4b919c000c463fe7268e
asksven <sven.knispel@gmail.com>
2017-11-26 17:45:15 +0100

    make sure that .circlerc is re-sourced

--- circleciscripts/decrypt_env_vars.sh
+++ circleciscripts/decrypt_env_vars.sh
@@ -4,10 +4,10 @@
 # openssl aes-256-cbc -e -in secret-env-plain -out secret-env-cipher -k KEY
 
 echo "Decrypting"
-openssl aes-256-cbc -d -md md5 -in secret-env-cipher -out secret-env-plain -k $KEY >> ~/.circlerc
+openssl aes-256-cbc -d -md md5 -in secret-env-cipher -k $KEY >> ~/.circlerc
 ls -l secret-*
-cat ./secret-env-plain
+cat ~/.circlerc
+source ~/.circlerc
 
 echo "Setting vars"
-source ./secret-env-plain
 echo $KEY_ALIAS
