60a77347301eb141b51b46da0690533fdfd2d045
asksven <sven.knispel@gmail.com>
2017-11-26 15:58:58 +0100

    Switched to relative path

--- circleciscripts/download_keystore.sh
+++ circleciscripts/download_keystore.sh
@@ -6,6 +6,6 @@ KEYSTORE_DEBUG=${KEYSTORE_DEBUG%$'\r'}
 curl -O ${KEYSTORE_URI}/${KEYSTORE_RELEASE}-cipher
 curl -O ${KEYSTORE_URI}/${KEYSTORE_DEBUG}-cipher
 
-if [ -e ${KEYSTORE_RELEASE}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> $HOME/unclutter/app/$KEYSTORE_RELEASE; fi
-if [ -e ${KEYSTORE_DEBUG}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> $HOME/unclutter/app/$KEYSTORE_DEBUG; fi
+if [ -e ${KEYSTORE_RELEASE}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_RELEASE}-cipher -k $KEY >> ./app/$KEYSTORE_RELEASE; fi
+if [ -e ${KEYSTORE_DEBUG}-cipher ]; then openssl aes-256-cbc -d -in ${KEYSTORE_DEBUG}-cipher -k $KEY >> ./app/$KEYSTORE_DEBUG; fi
 
