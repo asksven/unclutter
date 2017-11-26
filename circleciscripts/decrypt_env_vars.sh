46e637a953a6ad17b429bc6565367f76c00f57af
asksven <sven.knispel@gmail.com>
2017-11-26 17:21:47 +0100

    More tracing info

--- circleciscripts/decrypt_env_vars.sh
+++ circleciscripts/decrypt_env_vars.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+echo "Decrypting"
 openssl aes-256-cbc -d -md md5 -in secret-env-cipher -out secret-env-plain -k $KEY
-source secret-env-plain
+ls secret-*
+
+echo "Setting vars"
+source ./secret-env-plain
 echo $KEY_ALIAS
